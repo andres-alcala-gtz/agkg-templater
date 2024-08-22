7251f9fff476e745524687b832b775bcca1ba8fb
andres-alcala-gtz <andres.alcala.gtz@gmail.com>
2024-08-22 13:13:48 -0600

    v0.6.0

--- search.py
+++ search.py
@@ -3,6 +3,7 @@ import docx
 import pandas
 import pathlib
 import datetime
+import geopy.geocoders
 
 
 def search_part_style(path: pathlib.Path) -> dict[str, str]:
@@ -68,46 +69,78 @@ def search_data(data_frame: pandas.DataFrame, part_style: dict[str, str]) -> dic
     data = {}
 
     data["«identifier»"] = _loc(section="Plan Appraisal - Cover", label="AID:")
-    data["«target»"] = _loc(section="Plan Appraisal - Cover", label="Target:")
+    data["«target»"] = _loc(section="Plan Appraisal - Cover", label="Target:").replace("- ", "")
+    data["«TARGET»"] = data["«target»"].upper()
 
-    data["«timezone»"] = _loc(section="Plan Appraisal – Sample Scope: Appraisal Setup", label="Time Zone")
     data["«partner»"] = _loc(section="Plan Appraisal – Sample Scope: Appraisal Setup", label="Partner")
+    data["«PARTNER»"] = data["«partner»"].upper()
     data["«objectives»"] = _loc(section="Plan Appraisal – Sample Scope: Appraisal Setup", label="Business and Appraisal Objectives")
-    data["«virtual»"] = _loc(section="Plan Appraisal – Sample Scope: Appraisal Setup", label="Use of a virtual collection technique for appraisal")
+    _, data["«objectives_business»"], data["«objectives_appraisal»"] = re.split("\w+ objectives:\n", data["«objectives»"])
 
     data["«organization_name»"] = _loc(section="Plan Appraisal – Sample Scope: Organization", title="Organization", label="Name")
+    data["«ORGANIZATION_NAME»"] = data["«organization_name»"].upper()
     data["«organization_name_native»"] = _loc(section="Plan Appraisal – Sample Scope: Organization", title="Organization", label="Native Language Name")
+    data["«ORGANIZATION_NAME_NATIVE»"] = data["«organization_name_native»"].upper()
     data["«organization_city»"] = _loc(section="Plan Appraisal – Sample Scope: Organization", title="Organization", label="City")
     data["«organization_region»"] = _loc(section="Plan Appraisal – Sample Scope: Organization", title="Organization", label="State/Province/Region")
+    data["«organization_country»"] = geopy.geocoders.Nominatim(user_agent="templater").geocode(query=data["«organization_city»"], language="en").address.split(", ")[-1]
 
     data["«ou_name»"] = _loc(section="Plan Appraisal – Sample Scope: Organization", title="Organizational Unit (OU)", label="Name")
+    data["«OU_NAME»"] = data["«ou_name»"].upper()
     data["«ou_name_native»"] = _loc(section="Plan Appraisal – Sample Scope: Organization", title="Organizational Unit (OU)", label="Native Language Name")
+    data["«OU_NAME_NATIVE»"] = data["«ou_name_native»"].upper()
 
     data["«sponsor_name»"] = _loc(personnel=True, section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", text="Appraisal Sponsor")
+    data["«Sponsor_Name»"] = data["«sponsor_name»"].title()
     data["«sponsor_organization»"] = _loc(section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", subtitle=data["«sponsor_name»"], label="Organization")
 
     data["«ri_name»"] = _loc(personnel=True, section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", text="Registered Interpreter")
+    data["«RI_Name»"] = data["«ri_name»"].title()
     data["«ri_organization»"] = _loc(section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", subtitle=data["«ri_name»"], label="Organization")
 
     data["«ouc_name»"] = _loc(personnel=True, section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", text="OU Coordinator")
+    data["«OUC_Name»"] = data["«ouc_name»"].title()
     data["«ouc_organization»"] = _loc(section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", subtitle=data["«ouc_name»"], label="Organization")
 
     data["«atm0_name»"] = _loc(personnel=True, index=0, section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", text="Appraisal Team Member")
+    data["«ATM0_Name»"] = data["«atm0_name»"].title()
     data["«atm0_organization»"] = _loc(section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", subtitle=data["«atm0_name»"], label="Organization")
 
     data["«atm1_name»"] = _loc(personnel=True, index=1, section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", text="Appraisal Team Member")
+    data["«ATM1_Name»"] = data["«atm1_name»"].title()
     data["«atm1_organization»"] = _loc(section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", subtitle=data["«atm1_name»"], label="Organization")
 
     data["«atm2_name»"] = _loc(personnel=True, index=2, section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", text="Appraisal Team Member")
+    data["«ATM2_Name»"] = data["«atm2_name»"].title()
     data["«atm2_organization»"] = _loc(section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", subtitle=data["«atm2_name»"], label="Organization")
 
     data["«atm3_name»"] = _loc(personnel=True, index=3, section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", text="Appraisal Team Member")
+    data["«ATM3_Name»"] = data["«atm3_name»"].title()
     data["«atm3_organization»"] = _loc(section="Plan Appraisal - Sample Scope: Appraisal Personnel", title="Create Appraisal Team", subtitle=data["«atm3_name»"], label="Organization")
 
-    data["«plan_start_date»"] = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Plan Appraisal Start Date"), "%Y/%m/%d").strftime("%d-%b-%Y")
-    data["«plan_end_date»"] = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Plan Appraisal End Date"), "%Y/%m/%d").strftime("%d-%b-%Y")
-    data["«conduct_start_date»"] = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Conduct Appraisal Start Date"), "%Y/%m/%d").strftime("%d-%b-%Y")
-    data["«conduct_end_date»"] = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Conduct Appraisal End Date"), "%Y/%m/%d").strftime("%d-%b-%Y")
+    data["«pinyin»"] = "(pinyin)" if data["«organization_country»"] == "China" else ""
+    data["«Pinyin»"] = data["«pinyin»"].title()
+    data["«PINYIN»"] = data["«pinyin»"].upper()
+
+    plan_start = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Plan Appraisal Start Date"), "%Y/%m/%d")
+    plan_end = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Plan Appraisal End Date"), "%Y/%m/%d")
+    conduct_start = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Conduct Appraisal Start Date"), "%Y/%m/%d")
+    conduct_end = datetime.datetime.strptime(_loc(section="Plan Appraisal - Sample Scope: Timeline and Scheduling", label="Conduct Appraisal End Date"), "%Y/%m/%d")
+    expiration = conduct_end.replace(year=conduct_end.year + 3)
+
+    data["«plan_start_date»"] = plan_start.strftime("%d-%b-%Y")
+    data["«plan_end_date»"] = plan_end.strftime("%d-%b-%Y")
+    data["«conduct_start_date»"] = conduct_start.strftime("%d-%b-%Y")
+    data["«conduct_end_date»"] = conduct_end.strftime("%d-%b-%Y")
+    data["«expiration_date»"] = expiration.strftime("%d-%b-%Y")
+
+    data["«conduct_start_year»"] = conduct_start.strftime("%Y")
+    data["«conduct_start_month»"] = conduct_start.strftime("%m")
+    data["«conduct_start_day»"] = conduct_start.strftime("%d")
+
+    data["«conduct_end_year»"] = conduct_end.strftime("%Y")
+    data["«conduct_end_month»"] = conduct_end.strftime("%m")
+    data["«conduct_end_day»"] = conduct_end.strftime("%d")
 
     return data
 
